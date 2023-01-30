4984ed0e04e16d01f39242c2e59ee8d4a364bec6
Constantin <konstantindanilov19@hotmail.com>
2023-01-30 23:08:32 +0200

    finished making main logic of the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import { ProductList } from './pages/ProductList';
 
 const App = () => {
-  return <Routes>
-    <Route path='' element={<ProductList />}/>
-    <Route path='shirt-details' element={<ShirtDetails />}/>
-  </Routes>;
+  return (
+    <Routes>
+      <Route path="" element={<ProductList />} />
+      <Route path="shirt-details" element={<ShirtDetails />} />
+    </Routes>
+  );
 };
 
 export default App;
